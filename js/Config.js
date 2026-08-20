1d923462ba702ce3566eef7dce39bd889452f663
Maria Eduarda <mariaeduardamelo5512@gmail.com>
2026-08-20 09:06:05 -0300

    atualiza projeto

---
+++ js/Config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlPWkjOJ3dzRFhgISHyASVqMCoYdTniho",
+  authDomain: "projetotrilha-861da.firebaseapp.com",
+  projectId: "projetotrilha-861da",
+  storageBucket: "projetotrilha-861da.firebasestorage.app",
+  messagingSenderId: "187839105843",
+  appId: "1:187839105843:web:51007f1777b38131448c46"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
